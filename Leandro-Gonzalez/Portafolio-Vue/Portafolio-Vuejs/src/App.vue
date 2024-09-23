280d76c1586ca5426733f25e319c98d57d590f42
Leandro Gonzalez <leanlag@gmail.com>
2024-09-23 12:44:24 -0300

    Capitulo 5 portafolio Vue personal Mieducacion

--- Leandro-Gonzalez/Portafolio-Vue/Portafolio-Vuejs/src/App.vue
+++ Leandro-Gonzalez/Portafolio-Vue/Portafolio-Vuejs/src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import NavBar from './components/NavBar.vue'
 import DatosPersonales from './components/DatosPersonales.vue'
+import MiEducacion from './components/MiEducacion.vue';
 </script>
 
 <template>
@@ -12,7 +13,10 @@ import DatosPersonales from './components/DatosPersonales.vue'
   </header>
 
   <main>
-    <section id="educacion"><h2>Educación - Cursos</h2></section>
+    <section id="educacion">
+      <h2>Educación - Cursos</h2> 
+      <MiEducacion /> 
+    </section>
     <section id="experiencia"><h2>Experiencia</h2></section>
     <section id="proyectos"><h2>Proyectos</h2></section>
     <section id="habilidades"><h2>Habilidades</h2></section>
