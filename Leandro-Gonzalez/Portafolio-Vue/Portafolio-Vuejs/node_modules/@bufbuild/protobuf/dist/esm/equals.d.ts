8e369f893eb117f751529518c8804cef0d5a7ad8
Leandro Gonzalez <leanlag@gmail.com>
2024-09-23 15:14:09 -0300

    Modificacion en las carpetas

--- Leandro-Gonzalez/Portafolio-Vue/Portafolio-Vuejs/node_modules/@bufbuild/protobuf/dist/esm/equals.d.ts
+++
@@ -1,9 +0,0 @@
-import type { MessageShape } from "./types.js";
-import type { DescMessage } from "./descriptors.js";
-/**
- * Compare two messages of the same type.
- *
- * Note that this function disregards extensions and unknown fields, and that
- * NaN is not equal NaN, following the IEEE standard.
- */
-export declare function equals<Desc extends DescMessage>(schema: Desc, a: MessageShape<Desc>, b: MessageShape<Desc>): boolean;
