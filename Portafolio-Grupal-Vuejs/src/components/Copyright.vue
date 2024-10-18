cf7e27129c60ff1a9c053fd4baedf82a2f7aa58a
MartinianoZ <166861612+sarox23@users.noreply.github.com>
2024-10-17 22:52:13 -0300

    Add files via upload
    subo copyright portafolio grupal

---
+++ Portafolio-Grupal-Vuejs/src/components/Copyright.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="copyright">
+    © {{ year }} {{ companyName }}
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Copyright",
+    props: {
+    companyName: {
+        type: String,
+        default: "Mi Compañía"
+    }
+},
+data() {
+    return {
+    year: new Date().getFullYear()
+    };
+}
+};
+</script>
+
+<style scoped>
+.copyright {
+    font-size: 1rem;
+    color: #555;
+}
+</style>
