6a0c9ce6f1acd0b4e4d261ca998c05feee22bdd8
Leandro Gonzalez <leanlag@gmail.com>
2024-10-23 15:56:44 -0300

    Clase 10 Python POO Parte 7 Diseño, Constante y contexto estático

---
+++ Leandro-Gonzalez/Python/Clase-10/Constantes.py
@@ -0,0 +1,7 @@
+#Creamos una clase
+class Matematicas:
+    PI = 3.1416 #Variable de clase constante
+
+
+#Creamos una variable constante
+MI_CONSTANTE = "Esta es una variable constante"
--- Leandro-Gonzalez/Python/Clase-10/MiClase.py
+++ Leandro-Gonzalez/Python/Clase-10/MiClase.py
@@ -5,15 +5,50 @@ class MiClase:
     def __init__(self, variable_instancia): # La variable de instancia, da diferentes valores
         self.variable_instancia = variable_instancia
         
+#Agregamos un metodo estatico usando el decorador @staticmethod
+    @staticmethod
+    def metodo_estatico():
+        print("Metodo estatico")
+        print(MiClase.variable_clase)
+        
+#Agramos un decorador de clase, sirve para acceder a la variable de clase
+    @classmethod
+    def metodo_clase(cls): # cls es una referencia a la clase
+        print("Metodo de clase")
+        print(cls.variable_clase)        
+
+#Creamos un metodo de instancia
+    def metodo_instancia(self):
+        self.metodo_clase()
+        self.metodo_estatico()
+        print(self.variable_instancia)
+        print(self.variable_clase)
+        
 
 print(MiClase.variable_clase) # Variable de clase   
 
 #creamos un objeto
-miClase = MiClase("Variable de instancia")     
-print(miClase.variable_instancia) # Variable de instancia
-print(miClase.variable_clase) # Variable de clase
+miClase1 = MiClase("Variable de instancia")     
+print(miClase1.variable_instancia) # Variable de instancia
+print(miClase1.variable_clase) # Variable de clase
 #Creamos otro objeto
 miClase2 = MiClase("Otra variable de instancia")
 print(miClase2.variable_instancia) # Otra variable de instancia
 print(miClase2.variable_clase) # Variable de clase
 
+MiClase.variable_clase2 = "Variable de clase 2" # Se puede agregar una variable de clase
+print(MiClase.variable_clase2) # Variable de clase 2
+print(miClase1.variable_clase2) # Variable de clase 2
+print(miClase2.variable_clase2) # Variable de clase 2
+
+#Llamamos al metodo estatico
+MiClase.metodo_estatico() # Metodo estatico
+
+#Llamamos al metodo de clase
+MiClase.metodo_clase() # Metodo de clase
+
+#Creamos un objeto
+miObjeto1 = MiClase("Variable de instancia")
+miObjeto1.metodo_clase() # Metodo de clase
+miObjeto1.metodo_estatico() # Metodo estatico
+miObjeto1.metodo_instancia() # Metodo de instancia
