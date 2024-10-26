0bc3c38fbaaa35a381ddb4a8fd76d8c1c790ea13
Denis Taraboreli <dtaraboreli@gmail.com>
2024-10-26 12:56:53 -0300

    Agrego portafolio personal a mi rama

---
+++ Portafolio-Vuejs/src/App.vue
@@ -0,0 +1,52 @@
+<!--
+aqui se ejecurara el codigo que se encuentra en el scrip,
+permite que todo lo que pongamos dentro de script, utilizando doble 
+llave puede hacer que funcione de forma reactiva vue
+-->
+<script setup>
+//puro js
+import NavBar from './components/NavBar.vue';
+import DatosPersonales from './components/DatosPersonales.vue';
+import ListaEducacion from './components/ListaEducacion.vue';
+import ExperienciaComponente from './components/ExperienciaComponente.vue';
+import ProyectoComponente from './components/ProyectoComponente.vue';
+</script>
+
+
+
+
+
+<template> 
+<!--dentro del template podemos utilizar html puro
+ o JavaScript de forma reactiva de la forma que se hizo arriba-->
+  <header>
+    <!--Barra de navegacion-->
+    <NavBar />
+    <!--Componente DatosPersonales-->
+
+    <DatosPersonales />
+
+  </header>
+  <main>
+    <section id="educacion"><h2>Educación - Cursos</h2>
+      <!--Componente DatosPersonales-->
+      <ListaEducacion />
+    </section>
+    <section id="experiencia"><h2>Experiencia</h2>
+      <!--Componente DatosPersonales-->
+      <ExperienciaComponente />
+    </section>
+    <section id="proyectos"><h2>Proyectos</h2>
+      <!--Componente Proyectos-->
+      <ProyectoComponente />
+    </section>
+    
+    <section id="habilidades"><h2>Habilidades</h2></section>
+    <section id="intereses"><h2>Intereses</h2></section>
+  </main>
+</template>
+
+<style scoped>
+
+
+</style>
---
+++ Portafolio-Vuejs/src/components/DatosPersonales.vue
@@ -0,0 +1,95 @@
+<script setup>
+const title = "Denis Taraboreli";
+const descripcion = "Tecnico universitario en Programacion - UTN";
+const residencia="Pergamino, Buenos Aires, Argentina";
+const presentacion = "Hola, Bienvenido a mi portafolio de proyectos. Soy desarrollador web con experiencia en el desarrollo de aplicaciones web  y mobiles";
+const redesSociales = [
+    {id: 1, name: 'linkedin', src: 'src/assets/linkedin_icon.svg', url: ''},
+    {id: 2, name: 'GitHub', src: 'src/assets/github_icon.svg', url: ''},
+    {id: 3, name: 'Cullicum vitae', src: 'src/assets/cv_resume_icon.svg', url: ''}
+];
+const telefono = "+54 9 2477-380932"
+</script>
+
+<template>
+<section class="datos-personales">
+    <div class="card">
+        <h1>{{title}}</h1>
+        <h2>{{ descripcion }}</h2>
+        <P>{{ presentacion }}</P>
+        <ul class="container-lista">
+                <li v-for="red in redesSociales" :key="red.id">
+                    <a :href="red.url"><img class="icon-redsocial" :src="red.src" width="35rem" :alt="red.name" /></a>
+                </li>
+            </ul>
+        <h3> Mi telefono personal: {{ telefono }}</h3>
+        <h4> {{residencia}}</h4>
+    </div>
+
+</section>
+</template>
+
+
+<style scoped>
+.datos-personales {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    margin: 2rem;
+}
+
+.card {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    background-color: azure;
+    border-radius: 10px;
+    padding: 1rem;
+    margin: 1rem;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+
+.container-lista {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: row;
+    list-style: none;
+    padding: 0;
+}
+
+.intagrantes {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    list-style: none;
+    padding: 0;
+}
+
+.icon-redsocial {
+    margin: 0.5rem;
+}
+
+h1 {
+    font-size: 2rem;
+    color: #333;
+}
+
+h2 {
+    font-size: 1.5rem;
+    color: #333;
+}
+
+h4 {
+    font-size: 1rem;
+    color: #333;
+}
+
+p {
+    font-size: 1rem;
+    color: #333;
+}
+</style>
