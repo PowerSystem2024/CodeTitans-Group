640cc7baeea34a826ac4046c3a6f241fa6bc3bec
Leandro Gonzalez <leanlag@gmail.com>
2024-11-05 15:59:46 -0300

    Correcion en clase 14 python

--- Python/Clase-14/mundo_pc/test_mundo_pc.py
+++ Python/Clase-14/mundo_pc/test_mundo_pc.py
@@ -10,11 +10,16 @@ monitor1 = Monitor("HP", "15 Pulgadas") # type: ignore
 computadora1 = computadora("HP", monitor1, teclado1, raton1)
 
 
+
 teclado2 = Teclado("Acer", "Bluetooth") # type: ignore
 monitor2 = Monitor("Acer", "27 Pulgadas") # type: ignore
 raton2 = Raton("Acer", "USB") # type: ignore
 computadora2 = computadora("Acer", monitor2, teclado2, raton2)
 
-computadoras1 = [computadora1, computadoras2]
+computadoras1 = [computadora1]
 orden1 = Orden(computadoras1)
 print(orden1)
+
+computadoras2 = [computadora2]
+orden2 = Orden(computadoras2)
+print(orden2)
